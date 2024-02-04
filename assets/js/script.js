3f6873fb84bc6d55abf93c47ee056ffbe67c055d
IsItIzzet <ohlhjames@gmail.com>
2024-02-04 16:32:25 -0600

    added example javascript for tax informer idea

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+
+var urlName = 'google.com' /*document.querySelector('#url-name'); */
+var zipcode = document.querySelector('#zip-code'); 
+
+fetch('https://api.api-ninjas.com/v1/urllookup?url=' + url, {
+    method: 'GET',
+    headers: {
+      'X-Api-Key': 'GFxmJh+HYnvd21whXHNOdg==BrQLzDzNqIDIS3uU',
+    },
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data)
+    companyZip = data.zip;
+    console.log(companyZip)
+    fetch('https://api.api-ninjas.com/v1/salestax?zip_code=' + companyZip, {
+      method: 'GET',
+      headers: {
+        'X-Api-Key': 'GFxmJh+HYnvd21whXHNOdg==BrQLzDzNqIDIS3uU',
+    },
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data2) {
+      console.log(data2)
+      totalTaxRate = data2[0].total_rate;
+      console.log(totalTaxRate);
+    });
+});
+
+/* if(company.tax > .09 ){
+  company.tax.color= red
+}else if(company.tax < .071){
+  company.tax.color = green
+}else{
+  company.tax.color = yellow
+}
+'website.blahh?search=' + name+ "&tax" */
